b404ae80daf0c39939f441a3877ef1f2ef6e3108
bhargavireddy2412 <135250639+bhargavireddy2412@users.noreply.github.com>
2026-06-09 00:06:07 +0100

    m

--- src/main/java/com/qa/automation/framework/base/BaseTest.java
+++ src/main/java/com/qa/automation/framework/base/BaseTest.java
@@ -2,20 +2,19 @@ package com.qa.automation.framework.base;
 
 import com.qa.automation.framework.config.ConfigReader;
 import com.qa.automation.framework.drivers.DriverFactory;
-import com.qa.automation.framework.reports.ExtentManager;
-import org.openqa.selenium.WebDriver;
+import io.qameta.allure.Attachment;
+import org.openqa.selenium.*;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 public class BaseTest {
 
-    public WebDriver driver;
+    protected WebDriver driver;
     protected ConfigReader configReader;
 
     @BeforeMethod(alwaysRun = true)
     public void setup() {
-
-        System.out.println(">>> DRIVER INIT START");
 
         configReader = ConfigReader.getInstance();
 
@@ -24,18 +23,33 @@ public class BaseTest {
 
         DriverFactory.setDriver(driver);
 
-        DriverFactory.getDriver().manage().window().maximize();
-        DriverFactory.getDriver().get(configReader.getBaseUrl());
-
-        System.out.println(">>> DRIVER INIT END");
+        driver.manage().window().maximize();
+        driver.get(configReader.getBaseUrl());
     }
 
     @AfterMethod(alwaysRun = true)
-    public void tearDown() {
+    public void tearDown(ITestResult result) {
+
+        if (result.getStatus() == ITestResult.FAILURE) {
+            attachScreenshot();
+        }
 
         if (DriverFactory.getDriver() != null) {
             DriverFactory.getDriver().quit();
             DriverFactory.unload();
         }
+    }
+
+    @Attachment(value = "Failure Screenshot", type = "image/png")
+    public byte[] attachScreenshot() {
+
+        WebDriver driver = DriverFactory.getDriver();
+
+        if (driver == null) {
+            System.out.println("Driver NULL → screenshot skipped");
+            return new byte[0];
+        }
+
+        return ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
     }
 }
